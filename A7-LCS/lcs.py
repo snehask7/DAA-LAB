ffb2cb85024de51bb68e2b22797871d5cf980756
Sneha Sriram Kannan <52703275+snehask7@users.noreply.github.com>
2020-06-03 01:04:46 +0530

    Update lcs.py

--- A7-LCS/lcs.py
+++ A7-LCS/lcs.py
@@ -1,47 +1,34 @@
-#2. Implement an algorithm to backtrace the LCS itself.
 def lcs(a,b):
-	m=len(a)
-	n=len(b)
-	l=[[0 for i in range(n+1)] for j in range(m+1)]
-	for x in range(1,m+1):
-		for y in range(1,n+1):
-			if a[x-1]==b[y-1]:
-				l[x][y]=l[x-1][y-1]+1
-			else:
-				l[x][y]=max(l[x-1][y],l[x][y-1])
-	#print(l)
-	maxele=l[m][n]
-	seq=[]
-	while maxele>0:
-		ele=0
-		for i in range(1,m+1):
-			for j in range(1,n+1):
-				if l[i][j]==maxele:
-					ele=a[i-1]
-					print(ele)	
-					break
-			if ele!=0:
-				break
-			
-		seq.append(ele)
-		maxele=maxele-1
-	print(seq[::-1])
-	return l[m][n]
+  m=len(a)
+  n=len(b)
+  l=[[0 for i in range(n+1)] for j in range(m+1)]
+  for x in range(m):
+    for y in range(n):
+      if a[x]==b[y]:
+        l[x+1][y+1]=l[x][y]+1
+      else:
+        l[x+1][y+1]=max(l[x][y+1],l[x+1][y])
+  print(l)
+  maxele=l[m][n]
+  seq=[]
+  i=m
+  j=n
+  while i>0 and j>0:
+    if a[i-1]==b[j-1]:
+      seq.append(a[i-1])
+      maxele=maxele-1
+      i-=1
+      j-=1
+    else:
+      if l[i-1][j]<l[i][j-1]:
+        j=j-1
+      else:
+        i=i-1	
+  print(seq[::-1])
+  return l[m][n]
 				
 print("Enter the elements in array A: ")
 a=[int(i) for i in input().split()]
 print("Enter the elements in array B: ")
 b=[int(i) for i in input().split()]
 print("longest common subsequence length: ",lcs(a,b))
-
-"""
-OUTPUT
-
-Enter the elements in array A: 
-3 4 5 6 9
-Enter the elements in array B: 
-9 4 5
-5
-4
-
-"""
